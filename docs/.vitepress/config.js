9f01b75c4bc0a7c0ab2b709e1b233ab5065f7f0e
wangkai <kai150036@163.com>
2025-04-28 13:14:06 +0800

    简化Algolia配置并添加测试页面

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -129,52 +129,11 @@ export default {
         search: {
             provider: 'algolia',
             options: {
-                appId: process.env.ALGOLIA_APP_ID,
-                apiKey: process.env.ALGOLIA_API_KEY,
-                indexName: process.env.ALGOLIA_INDEX_NAME,
-                locales: {
-                    root: {
-                        placeholder: '搜索文档',
-                        translations: {
-                            button: {
-                                buttonText: '搜索',
-                                buttonAriaLabel: '搜索'
-                            },
-                            modal: {
-                                searchBox: {
-                                    resetButtonTitle: '清除查询',
-                                    resetButtonAriaLabel: '清除查询',
-                                    cancelButtonText: '取消',
-                                    cancelButtonAriaLabel: '取消'
-                                },
-                                startScreen: {
-                                    recentSearchesTitle: '搜索历史',
-                                    noRecentSearchesText: '没有搜索历史',
-                                    saveRecentSearchButtonTitle: '保存至搜索历史',
-                                    removeRecentSearchButtonTitle: '从搜索历史中移除',
-                                    favoriteSearchesTitle: '收藏',
-                                    removeFavoriteSearchButtonTitle: '从收藏中移除'
-                                },
-                                errorScreen: {
-                                    titleText: '无法获取结果',
-                                    helpText: '你可能需要检查你的网络连接'
-                                },
-                                footer: {
-                                    selectText: '选择',
-                                    navigateText: '切换',
-                                    closeText: '关闭',
-                                    searchByText: '搜索提供者'
-                                },
-                                noResultsScreen: {
-                                    noResultsText: '无法找到相关结果',
-                                    suggestedQueryText: '你可以尝试查询',
-                                    reportMissingResultsText: '你认为该查询应该有结果？',
-                                    reportMissingResultsLinkText: '点击反馈'
-                                }
-                            }
-                        }
-                    }
-                }
+                appId: '10UPAJMD8Z',
+                apiKey: '317f87d8cda382394f70f0abab6f103f',
+                indexName: 'test_front_end_qingjiai_com_10upajmd8z_pages',
+                placeholder: '搜索文档',
+                // container: '#algolia-search'
             }
         }
     }
